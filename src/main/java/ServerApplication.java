20e23b3d3bde6f038153b1bc1b28e5ec90e97d50
zyjcxc <174866687@qq.com>
2018-07-03 16:57:23 +0800

    初次

---
+++ src/main/java/ServerApplication.java
@@ -0,0 +1,26 @@
+package com.meishu.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.jms.annotation.EnableJms;
+
+/**
+ * 启动类
+ * 
+ * @author mengqa
+ *
+ *         2017年4月18日
+ */
+//@EnableJms
+@SpringBootApplication
+@MapperScan(basePackages = {"com.meishu.admin.server.dao", "com.meishu.admin.finger.dao"})
+@Slf4j
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
